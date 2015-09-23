3411483be2f768a9be6e236484eecfc9a1413610
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-09-23 14:19:50 -0500

    Delete irrelevant methods and tests

--- lib/coin_changer.rb
+++ lib/coin_changer.rb
@@ -1,7 +1,6 @@
 class CoinChanger
 
-  attr_accessor :change
-  attr_reader :coins, :purse
+  attr_accessor :change, :coins, :purse
 
   def initialize(init_change)
     @change = init_change
@@ -12,9 +11,33 @@ class CoinChanger
   def changer
     coins.each do |key, value|
       num_coins = (@change / value)
-      @change %= value
+      @change = @change % value
       purse[key] = num_coins
     end
+    purse
+  end
+
+  def pennies
+    num_coins = (change / 1)
+    purse[:pennies] = num_coins
+    purse
+  end
+
+  def nickels
+    num_coins = (change / 5)
+    purse[:nickels] = num_coins
+    purse
+  end
+
+  def dimes
+    num_coins = (change / 10)
+    purse[:dimes] = num_coins
+    purse
+  end
+
+  def quarters
+    num_coins = (change / 25)
+    purse[:quarters] = num_coins
     purse
   end
 end
