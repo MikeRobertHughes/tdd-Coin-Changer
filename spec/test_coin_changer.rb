4c272d5285cedb519e8c5eb6194528455adc883d
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-09-23 14:25:15 -0500

    Refactor code and delete irrelevant methods and tests

--- spec/test_coin_changer.rb
+++ spec/test_coin_changer.rb
@@ -19,33 +19,9 @@ class TestCoinChanger < Minitest::Test
     assert_equal(99, coinage)
   end
 
-  def test_returns_pennies
-    changer = CoinChanger.new(99)
-    coinage = changer.pennies
-    assert_equal({pennies: 99}, coinage)
-  end
-
-  def test_returns_nickels
-    changer = CoinChanger.new(99)
-    coinage = changer.nickels
-    assert_equal({nickels: 19}, coinage)
-  end
-
-  def test_returns_dimes
-    changer = CoinChanger.new(99)
-    coinage = changer.dimes
-    assert_equal({dimes: 9}, coinage)
-  end
-
-  def test_returns_quarters
-    changer = CoinChanger.new(99)
-    coinage = changer.quarters
-    assert_equal({quarters: 3}, coinage)
-  end
-
   def test_coin_changer_returns_correct_change
     changer = CoinChanger.new(41)
-    coinage = changer.change
+    coinage = changer.changer
     assert_equal({quarters: 1, dimes: 1, nickels: 1, pennies: 1}, coinage)
   end
 
